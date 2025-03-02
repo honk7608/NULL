4bf056c2ceaa1a2e2acb591e0996c316b59f0db6
honk7608 <honk7608@gmail.com>
2025-03-02 19:38:12 +0900

    Fix

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -1,6 +1,7 @@
 count = 0
 titleEl = document.getElementsByClassName("hero-title")[0]
 titleEl.onclick = () => {
+    count++
     if(count == 100) {
         titleEl.innerHTML = "이 화면을 스크린샷을 이용해서<br>지원 서류에 첨부해주세요!"
         // 코드 스크린 샷은 절반만 인정합니다. 웹 상에서 메세지를 띄워보세요!
